01c61d178c1b2dd0ae4a166f2df4a2f9d11a6e6c
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-04-07 22:22:48 +0530

    port update for http

--- config/releases.exs
+++ config/releases.exs
@@ -1,6 +1,8 @@
 import Config
 
+http_port = System.get_env("HTTP_PORT") || 4000
+
 config :dbservice, DbserviceWeb.Endpoint,
   server: true,
-  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+  http: [port: http_port], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
