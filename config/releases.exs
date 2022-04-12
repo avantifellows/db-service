4effab2c7bb3ff37215b0f983f933975be951d65
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-04-12 16:58:52 +0530

    removed releases.exs

--- config/releases.exs
+++
@@ -1,9 +0,0 @@
-import Config
-
-http_port = System.get_env("HTTP_PORT") || 4000
-
-# gigalixir config
-config :dbservice, DbserviceWeb.Endpoint,
-  server: true,
-  http: [port: http_port], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
