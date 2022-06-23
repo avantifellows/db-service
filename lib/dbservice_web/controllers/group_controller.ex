ef5dfaa748a6bf66668398671fb6e3d10335b2f8
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-06-23 17:15:57 +0530

    group swagger + maps

--- lib/dbservice_web/controllers/group_controller.ex
+++ lib/dbservice_web/controllers/group_controller.ex
@@ -6,9 +6,62 @@ defmodule DbserviceWeb.GroupController do
 
   action_fallback DbserviceWeb.FallbackController
 
+  use PhoenixSwagger
+
+  def swagger_definitions do
+    %{
+      Group:
+        swagger_schema do
+          title("Group")
+          description("A group in the application")
+
+          properties do
+            input_schema(:map, "Input schema")
+            locale(:string, "The configured locale for the group")
+            locale_data(:map, "Meta data about locale settings for the group")
+          end
+
+          example(%{
+            input_schema: %{},
+            locale: "hi",
+            locale_data: %{
+              "hi" => %{
+                "title" => "सत्र के लिए पंजीकरण करें"
+              },
+              "en" => %{
+                "title" => "Register for session"
+              },
+            }
+          })
+        end,
+      Groups:
+        swagger_schema do
+          title("Groups")
+          description("All the groups")
+          type(:array)
+          items(Schema.ref(:Group))
+        end,
+    }
+  end
+
+  swagger_path :index do
+    get("/api/group")
+    response(200, "OK", Schema.ref(:Groups))
+  end
+
   def index(conn, _params) do
     group = Groups.list_group()
     render(conn, "index.json", group: group)
+  end
+
+  swagger_path :create do
+    post("/api/group")
+
+    parameters do
+      body(:body, Schema.ref(:Group), "Group to create", required: true)
+    end
+
+    response(201, "Created", Schema.ref(:Group))
   end
 
   def create(conn, params) do
@@ -20,9 +73,30 @@ defmodule DbserviceWeb.GroupController do
     end
   end
 
+  swagger_path :show do
+    get("/api/group/{groupId}")
+
+    parameters do
+      groupId(:path, :integer, "The id of the group", required: true)
+    end
+
+    response(200, "OK", Schema.ref(:Group))
+  end
+
   def show(conn, %{"id" => id}) do
     group = Groups.get_group!(id)
     render(conn, "show.json", group: group)
+  end
+
+  swagger_path :update do
+    patch("/api/group/{groupId}")
+
+    parameters do
+      groupId(:path, :integer, "The id of the group", required: true)
+      body(:body, Schema.ref(:Group), "Group to create", required: true)
+    end
+
+    response(200, "Updated", Schema.ref(:Group))
   end
 
   def update(conn, params) do
@@ -31,6 +105,16 @@ defmodule DbserviceWeb.GroupController do
     with {:ok, %Group{} = group} <- Groups.update_group(group, params) do
       render(conn, "show.json", group: group)
     end
+  end
+
+  swagger_path :delete do
+    PhoenixSwagger.Path.delete("/api/group/{groupId}")
+
+    parameters do
+      groupId(:path, :integer, "The id of the group", required: true)
+    end
+
+    response(204, "No Content")
   end
 
   def delete(conn, %{"id" => id}) do
