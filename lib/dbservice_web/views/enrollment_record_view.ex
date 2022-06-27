0a1d4ddd09c180bb660a96df2e3a09c0b3f6f601
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-06-27 22:26:19 +0530

    enrollment record student id and school id

--- lib/dbservice_web/views/enrollment_record_view.ex
+++ lib/dbservice_web/views/enrollment_record_view.ex
@@ -15,7 +15,9 @@ defmodule DbserviceWeb.EnrollmentRecordView do
       id: enrollment_record.id,
       grade: enrollment_record.grade,
       academic_year: enrollment_record.academic_year,
-      is_current: enrollment_record.is_current
+      is_current: enrollment_record.is_current,
+      student_id: enrollment_record.student_id,
+      school_id: enrollment_record.school_id
     }
   end
 end
