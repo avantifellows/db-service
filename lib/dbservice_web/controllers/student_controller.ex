2569b4b5cfc6c76eae6d57e5d14ebf6a2ed99bad
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-07-21 19:22:09 +0530

    swagger api for student registration

--- lib/dbservice_web/controllers/student_controller.ex
+++ lib/dbservice_web/controllers/student_controller.ex
@@ -39,6 +39,96 @@ defmodule DbserviceWeb.StudentController do
             group_id: 2
           })
         end,
+      StudentRegistration:
+        swagger_schema do
+          title("Student Registration")
+          description("A student in the application along with user info")
+
+          properties do
+            uuid(:string, "UUID for the student")
+            father_name(:string, "Father's name")
+            father_phone(:string, "Father's phone number")
+            mother_name(:string, "Mother's name")
+            mother_phone(:string, "Mother's phone number")
+            category(:string, "Category")
+            stream(:string, "Stream")
+            group_id(:integer, "Group ID for the student")
+            first_name(:string, "First name")
+            last_name(:string, "Last name")
+            email(:string, "Email")
+            phone(:string, "Phone number")
+            gender(:string, "Gender")
+            address(:string, "Address")
+            city(:string, "City")
+            district(:string, "District")
+            state(:string, "State")
+            pincode(:string, "Pin code")
+            role(:string, "User role")
+          end
+
+          example(%{
+            uuid: "120180101057",
+            father_name: "Narayan Pandey",
+            father_phone: "8989898989",
+            mother_name: "Lakshmi Pandey",
+            mother_phone: "9998887777",
+            category: "general",
+            stream: "PCB",
+            group_id: 2,
+            first_name: "Rahul",
+            last_name: "Sharma",
+            email: "rahul.sharma@example.com",
+            phone: "9998887777",
+            gender: "Male",
+            address: "Bandra Complex, Kurla Road",
+            city: "Mumbai",
+            district: "Mumbai",
+            state: "Maharashtra",
+            pincode: "400011",
+            role: "student"
+          })
+        end,
+      StudentWithUser:
+        swagger_schema do
+          title("Student with User")
+          description("A student in the application along with user info")
+
+          properties do
+            uuid(:string, "UUID for the student")
+            father_name(:string, "Father's name")
+            father_phone(:string, "Father's phone number")
+            mother_name(:string, "Mother's name")
+            mother_phone(:string, "Mother's phone number")
+            category(:string, "Category")
+            stream(:string, "Stream")
+            group_id(:integer, "Group ID for the student")
+            user(:map, "User details associated with the student")
+          end
+
+          example(%{
+            uuid: "120180101057",
+            father_name: "Narayan Pandey",
+            father_phone: "8989898989",
+            mother_name: "Lakshmi Pandey",
+            mother_phone: "9998887777",
+            category: "general",
+            stream: "PCB",
+            group_id: 2,
+            user: %{
+              first_name: "Rahul",
+              last_name: "Sharma",
+              email: "rahul.sharma@example.com",
+              phone: "9998887777",
+              gender: "Male",
+              address: "Bandra Complex, Kurla Road",
+              city: "Mumbai",
+              district: "Mumbai",
+              state: "Maharashtra",
+              pincode: "400011",
+              role: "student"
+            },
+          })
+        end,
       Students:
         swagger_schema do
           title("Students")
@@ -128,6 +218,16 @@ defmodule DbserviceWeb.StudentController do
     with {:ok, %Student{}} <- Users.delete_student(student) do
       send_resp(conn, :no_content, "")
     end
+  end
+
+  swagger_path :register do
+    post("/api/student/register")
+
+    parameters do
+      body(:body, Schema.ref(:StudentRegistration), "Student to create along with user", required: true)
+    end
+
+    response(201, "Created", Schema.ref(:StudentWithUser))
   end
 
   def register(conn, params) do
