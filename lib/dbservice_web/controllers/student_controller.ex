96f898a87b0b50e2974f71d4365caea041a8287a
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-07-25 15:43:41 +0530

    swagger def separated for student

--- lib/dbservice_web/controllers/student_controller.ex
+++ lib/dbservice_web/controllers/student_controller.ex
@@ -8,135 +8,20 @@ defmodule DbserviceWeb.StudentController do
 
   use PhoenixSwagger
 
+  alias DbserviceWeb.SwaggerSchema.Student, as: SwaggerSchemaStudent
+
   def swagger_definitions do
-    %{
-      Student:
-        swagger_schema do
-          title("Student")
-          description("A student in the application")
-
-          properties do
-            uuid(:string, "UUID for the student")
-            father_name(:string, "Father's name")
-            father_phone(:string, "Father's phone number")
-            mother_name(:string, "Mother's name")
-            mother_phone(:string, "Mother's phone number")
-            category(:string, "Category")
-            stream(:string, "Stream")
-            user_id(:integer, "User ID for the student")
-            group_id(:integer, "Group ID for the student")
-          end
-
-          example(%{
-            uuid: "120180101057",
-            father_name: "Narayan Pandey",
-            father_phone: "8989898989",
-            mother_name: "Lakshmi Pandey",
-            mother_phone: "9998887777",
-            category: "general",
-            stream: "PCB",
-            user_id: 1,
-            group_id: 2
-          })
-        end,
-      StudentRegistration:
-        swagger_schema do
-          title("Student Registration")
-          description("A student in the application along with user info")
-
-          properties do
-            uuid(:string, "UUID for the student")
-            father_name(:string, "Father's name")
-            father_phone(:string, "Father's phone number")
-            mother_name(:string, "Mother's name")
-            mother_phone(:string, "Mother's phone number")
-            category(:string, "Category")
-            stream(:string, "Stream")
-            group_id(:integer, "Group ID for the student")
-            first_name(:string, "First name")
-            last_name(:string, "Last name")
-            email(:string, "Email")
-            phone(:string, "Phone number")
-            gender(:string, "Gender")
-            address(:string, "Address")
-            city(:string, "City")
-            district(:string, "District")
-            state(:string, "State")
-            pincode(:string, "Pin code")
-            role(:string, "User role")
-          end
-
-          example(%{
-            uuid: "120180101057",
-            father_name: "Narayan Pandey",
-            father_phone: "8989898989",
-            mother_name: "Lakshmi Pandey",
-            mother_phone: "9998887777",
-            category: "general",
-            stream: "PCB",
-            group_id: 2,
-            first_name: "Rahul",
-            last_name: "Sharma",
-            email: "rahul.sharma@example.com",
-            phone: "9998887777",
-            gender: "Male",
-            address: "Bandra Complex, Kurla Road",
-            city: "Mumbai",
-            district: "Mumbai",
-            state: "Maharashtra",
-            pincode: "400011",
-            role: "student"
-          })
-        end,
-      StudentWithUser:
-        swagger_schema do
-          title("Student with User")
-          description("A student in the application along with user info")
-
-          properties do
-            uuid(:string, "UUID for the student")
-            father_name(:string, "Father's name")
-            father_phone(:string, "Father's phone number")
-            mother_name(:string, "Mother's name")
-            mother_phone(:string, "Mother's phone number")
-            category(:string, "Category")
-            stream(:string, "Stream")
-            group_id(:integer, "Group ID for the student")
-            user(:map, "User details associated with the student")
-          end
-
-          example(%{
-            uuid: "120180101057",
-            father_name: "Narayan Pandey",
-            father_phone: "8989898989",
-            mother_name: "Lakshmi Pandey",
-            mother_phone: "9998887777",
-            category: "general",
-            stream: "PCB",
-            group_id: 2,
-            user: %{
-              first_name: "Rahul",
-              last_name: "Sharma",
-              email: "rahul.sharma@example.com",
-              phone: "9998887777",
-              gender: "Male",
-              address: "Bandra Complex, Kurla Road",
-              city: "Mumbai",
-              district: "Mumbai",
-              state: "Maharashtra",
-              pincode: "400011",
-              role: "student"
-            },
-          })
-        end,
-      Students:
-        swagger_schema do
-          title("Students")
-          description("All students in the application")
-          type(:array)
-          items(Schema.ref(:Student))
-        end
-    }
+    # merge the required definitions in a pair at a time using the Map.merge/2 function
+    Map.merge(
+      Map.merge(
+        SwaggerSchemaStudent.student(),
+        SwaggerSchemaStudent.students()
+      ),
+      Map.merge(
+        SwaggerSchemaStudent.student_registration(),
+        SwaggerSchemaStudent.student_with_user()
+      )
+    )
   end
 
   swagger_path :index do
