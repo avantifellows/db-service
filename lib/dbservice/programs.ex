ffcc27dad582b9b0034d2cf5e148dae4717d468d
Bahugunajii <amanbahuguna009@gmail.com>
2022-08-17 17:59:52 +0530

    CI check fixes

--- lib/dbservice/programs.ex
+++ lib/dbservice/programs.ex
@@ -9,12 +9,12 @@ defmodule Dbservice.Programs do
 
   @doc """
   Returns the list of program.
-  
+
   ## Examples
-  
+
       iex> list_program()
       [%program{}, ...]
-  
+
   """
   def list_program do
     Repo.all(Program)
@@ -22,31 +22,31 @@ defmodule Dbservice.Programs do
 
   @doc """
   Gets a single program.
-  
+
   Raises `Ecto.NoResultsError` if the Program does not exist.
-  
+
   ## Examples
-  
+
       iex> get_program!(123)
       %program{}
-  
+
       iex> get_program!(456)
       ** (Ecto.NoResultsError)
-  
+
   """
   def get_program!(id), do: Repo.get!(Program, id)
 
   @doc """
   Creates a program.
-  
+
   ## Examples
-  
+
       iex> create_program(%{field: value})
       {:ok, %program{}}
-  
+
       iex> create_program(%{field: bad_value})
       {:error, %Ecto.Changeset{}}
-  
+
   """
   def create_program(attrs \\ %{}) do
     %Program{}
@@ -56,15 +56,15 @@ defmodule Dbservice.Programs do
 
   @doc """
   Updates a program.
-  
+
   ## Examples
-  
+
       iex> update_program(program, %{field: new_value})
       {:ok, %program{}}
-  
+
       iex> update_program(program, %{field: bad_value})
       {:error, %Ecto.Changeset{}}
-  
+
   """
   def update_program(%Program{} = program, attrs) do
     program
@@ -74,15 +74,15 @@ defmodule Dbservice.Programs do
 
   @doc """
   Deletes a program.
-  
+
   ## Examples
-  
+
       iex> delete_program(program)
       {:ok, %program{}}
-  
+
       iex> delete_program(program)
       {:error, %Ecto.Changeset{}}
-  
+
   """
   def delete_program(%Program{} = program) do
     Repo.delete(program)
@@ -90,12 +90,12 @@ defmodule Dbservice.Programs do
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking program changes.
-  
+
   ## Examples
-  
+
       iex> change_program(program)
       %Ecto.Changeset{data: %program{}}
-  
+
   """
   def change_Program(%Program{} = program, attrs \\ %{}) do
     Program.changeset(program, attrs)
