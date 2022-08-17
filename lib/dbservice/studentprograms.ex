1fbe2fa25212ec8d766e2ca23009d42475db4bd9
Bahugunajii <amanbahuguna009@gmail.com>
2022-08-17 18:02:22 +0530

    CI check fixes

--- lib/dbservice/studentprograms.ex
+++ lib/dbservice/studentprograms.ex
@@ -9,12 +9,12 @@ defmodule Dbservice.StudentPrograms do
 
   @doc """
   Returns the list of student_program.
-  
+
   ## Examples
-  
+
       iex> list_student_program()
       [%student_program{}, ...]
-  
+
   """
   def list_studentprogram do
     Repo.all(StudentProgram)
@@ -22,31 +22,31 @@ defmodule Dbservice.StudentPrograms do
 
   @doc """
   Gets a single student_program.
-  
+
   Raises `Ecto.NoResultsError` if the student_program does not exist.
-  
+
   ## Examples
-  
+
       iex> get_student_program!(123)
       %student_program{}
-  
+
       iex> get_student_program!(456)
       ** (Ecto.NoResultsError)
-  
+
   """
   def get_studentprogram!(id), do: Repo.get!(StudentProgram, id)
 
   @doc """
   Creates a student_program.
-  
+
   ## Examples
-  
+
       iex> create_student_program(%{field: value})
       {:ok, %student_program{}}
-  
+
       iex> create_student_program(%{field: bad_value})
       {:error, %Ecto.Changeset{}}
-  
+
   """
   def create_studentprogram(attrs \\ %{}) do
     %StudentProgram{}
@@ -56,15 +56,15 @@ defmodule Dbservice.StudentPrograms do
 
   @doc """
   Updates a student_program.
-  
+
   ## Examples
-  
+
       iex> update_student_program(student_program, %{field: new_value})
       {:ok, %student_program{}}
-  
+
       iex> update_student_program(student_program, %{field: bad_value})
       {:error, %Ecto.Changeset{}}
-  
+
   """
   def update_studentprogram(%StudentProgram{} = student_program, attrs) do
     student_program
@@ -74,15 +74,15 @@ defmodule Dbservice.StudentPrograms do
 
   @doc """
   Deletes a student_program.
-  
+
   ## Examples
-  
+
       iex> delete_student_program(student_program)
       {:ok, %student_program{}}
-  
+
       iex> delete_student_program(student_program)
       {:error, %Ecto.Changeset{}}
-  
+
   """
   def delete_studentprogram(%StudentProgram{} = student_program) do
     Repo.delete(student_program)
@@ -90,12 +90,12 @@ defmodule Dbservice.StudentPrograms do
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking student_program changes.
-  
+
   ## Examples
-  
+
       iex> change_student_program(student_program)
       %Ecto.Changeset{data: %student_program{}}
-  
+
   """
   def change_studentProgram(%StudentProgram{} = student_program, attrs \\ %{}) do
     StudentProgram.changeset(student_program, attrs)
