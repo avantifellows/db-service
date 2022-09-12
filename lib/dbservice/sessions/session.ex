efd8696373924e0f02c510c930672acec4553c06
Bahugunajii <amanbahuguna009@gmail.com>
2022-09-12 13:04:23 +0530

    CI fixes

--- lib/dbservice/sessions/session.ex
+++ lib/dbservice/sessions/session.ex
@@ -57,4 +57,10 @@ defmodule Dbservice.Sessions.Session do
       :meta_data
     ])
   end
+
+  def changeset_update_batches(session, batches) do
+    session
+    |> change()
+    |> put_assoc(:batches, batches)
+  end
 end
