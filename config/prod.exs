8d156b24b2430bb6bde3d1d7e89663bc5b2f7ae4
Bahugunajii <amanbahuguna009@gmail.com>
2022-10-04 12:52:53 +0530

    enforcing SSL

--- config/prod.exs
+++ config/prod.exs
@@ -47,3 +47,6 @@ config :logger, level: :info
 #       force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
+
+config :dbservice, DbserviceWeb.Endpoint,
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
