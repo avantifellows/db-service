c1d8610dba7d984434f88146e16c26f258db6e1b
Bahugunajii <amanbahuguna009@gmail.com>
2022-12-01 10:12:09 +0530

    setup production deploys

--- config/prod.exs
+++ config/prod.exs
@@ -49,3 +49,17 @@ config :logger, level: :info
 # Check `Plug.SSL` for all available options in `force_ssl`.
 
 config :dbservice, DbserviceWeb.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_proto]]
+
+config :dbservice, DbserviceWeb.Endpoint,
+  # Possibly not needed, but doesn't hurt
+  http: [port: {:system, "PORT"}],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
+
+config :dbservice, Dbservice.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  ssl: true,
+  # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
+  pool_size: 10
