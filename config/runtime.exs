f3dbac7a874b88636045e422d3ca99225d18433c
Bahugunajii <amanbahuguna009@gmail.com>
2022-12-14 17:09:09 +0530

    set timeout time

--- config/runtime.exs
+++ config/runtime.exs
@@ -53,7 +53,8 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    timeout: 120_000
 
   # ## Using releases
   #
