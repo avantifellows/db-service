a48f0429edd034d93714c4b5218caaabdcca2bab
Bahugunajii <amanbahuguna009@gmail.com>
2022-12-29 22:22:58 +0530

    added ip whitelisting plug to router

--- lib/dbservice_web/endpoint.ex
+++ lib/dbservice_web/endpoint.ex
@@ -45,10 +45,5 @@ defmodule DbserviceWeb.Endpoint do
   plug Plug.Head
   plug Plug.Session, @session_options
 
-  plug Plug.IpWhitelist.IpWhitelistEnforcer,
-    ip_whitelist: [{{127, 0, 0, 1}, {127, 0, 0, 1}}, {{35, 190, 11, 178}, {35, 190, 11, 178}}],
-    response_code_when_blacklisted: 401,
-    response_body_when_blacklisted: "Not Authenticated"
-
   plug DbserviceWeb.Router
 end
