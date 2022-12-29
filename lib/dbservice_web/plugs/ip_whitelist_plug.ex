b1ccf88132d7f49b25e0370042964c0071643912
Bahugunajii <amanbahuguna009@gmail.com>
2022-12-30 00:50:21 +0530

    added moduledoc tag in custom plug

--- lib/dbservice_web/plugs/ip_whitelist_plug.ex
+++ lib/dbservice_web/plugs/ip_whitelist_plug.ex
@@ -1,4 +1,11 @@
 defmodule DbserviceWeb.IpWhitelistPlug do
+  @moduledoc """
+    Only allow requests from the list of IP addresses specified. Assumes the request ip is present in the `remote_ip`
+    attribute on the passed in plug.
+    If the request IP is not whitelisted, the specified response code and body
+      will be added to the Plug.Conn and it will be halted.
+    If the request IP is on the whitelist, the plug chain will continue
+  """
   import Plug.Conn
 
   def init(options) do
