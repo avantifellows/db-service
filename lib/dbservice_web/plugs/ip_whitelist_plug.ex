fc403f18ec120fdcfb6d81ec245b884664acf995
Bahugunajii <amanbahuguna009@gmail.com>
2022-12-30 01:00:56 +0530

    minor changes

--- lib/dbservice_web/plugs/ip_whitelist_plug.ex
+++ lib/dbservice_web/plugs/ip_whitelist_plug.ex
@@ -21,7 +21,7 @@ defmodule DbserviceWeb.IpWhitelistPlug do
   end
 
   defp allowed_ip?(conn) do
-    allowed_ips = ['127.0.0.1', '35.190.11.178']
+    allowed_ips = ['127.0.0.1', "127.0.0.1", "35.190.11.178", '35.190.11.178']
     ip = :inet_parse.ntoa(conn.remote_ip)
     Enum.member?(allowed_ips, ip)
   end
