df3399e36522ac8da9414e038244dc971a5e2c00
Bahugunajii <amanbahuguna009@gmail.com>
2022-12-29 12:43:22 +0530

    removed remote ip dependency

--- lib/dbservice_web/endpoint.ex
+++ lib/dbservice_web/endpoint.ex
@@ -1,8 +1,6 @@
 defmodule DbserviceWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :dbservice
 
-  plug RemoteIp
-
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
