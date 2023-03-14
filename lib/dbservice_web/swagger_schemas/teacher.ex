f7232d2ccdfc9b29ca00823d330b4db3bd59e318
Bahugunajii <amanbahuguna009@gmail.com>
2023-03-14 11:25:52 +0530

    added swagger docs for teacher with user

--- lib/dbservice_web/swagger_schemas/teacher.ex
+++ lib/dbservice_web/swagger_schemas/teacher.ex
@@ -44,4 +44,90 @@ defmodule DbserviceWeb.SwaggerSchema.Teacher do
         end
     }
   end
+
+  def teacher_registration do
+    %{
+      TeacherRegistration:
+        swagger_schema do
+          title("Teacher Registration")
+          description("A teacher in the application along with user info")
+
+          properties do
+            designation(:string, "Designation for the teacher")
+            grade(:string, "Grade level in which a teacher instructs")
+            subject(:string, "Subject taught by the teacher")
+            uuid(:string, "UUID of the teacher")
+            school_id(:integer, "ID of the school associated with the teacher")
+            full_name(:string, "Full name")
+            email(:string, "Email")
+            phone(:string, "Phone number")
+            gender(:string, "Gender")
+            address(:string, "Address")
+            city(:string, "City")
+            district(:string, "District")
+            state(:string, "State")
+            pincode(:string, "Pin code")
+            role(:string, "User role")
+          end
+
+          example(%{
+            designation: "Principal",
+            grade: "High School",
+            uuid: "AF419",
+            category: "general",
+            stream: "PCB",
+            full_name: "Aman Bahuguna",
+            email: "aman.bahuguna@example.com",
+            school_id: 2,
+            phone: "8484515848",
+            gender: "Male",
+            address: "Bandra Complex, Kurla Road",
+            city: "Mumbai",
+            district: "Mumbai",
+            state: "Maharashtra",
+            pincode: "400011",
+            role: "principal"
+          })
+        end
+    }
+  end
+
+  def teacher_with_user do
+    %{
+      TeacherWithUser:
+        swagger_schema do
+          title("Teacher with User")
+          description("A teacher in the application along with user info")
+
+          properties do
+            designation(:string, "Designation for the teacher")
+            grade(:string, "Grade level in which a teacher instructs")
+            subject(:string, "Subject taught by the teacher")
+            uuid(:string, "UUID of the teacher")
+            user(:map, "User details associated with the teacher")
+          end
+
+          example(%{
+            designation: "Principal",
+            grade: "High School",
+            uuid: "AF419",
+            category: "general",
+            stream: "PCB",
+            school_id: 2,
+            user: %{
+              full_name: "Aman Bahuguna",
+              email: "aman.bahuguna@example.com",
+              phone: "8484515848",
+              gender: "Male",
+              address: "Bandra Complex, Kurla Road",
+              city: "Mumbai",
+              district: "Mumbai",
+              state: "Maharashtra",
+              pincode: "400011",
+              role: "principal"
+            }
+          })
+        end
+    }
+  end
 end
