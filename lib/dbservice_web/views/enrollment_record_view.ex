143cc8d62de85210dee90ae70876b4dc637fe22d
Bahugunajii <amanbahuguna009@gmail.com>
2023-03-24 15:35:15 +0530

    created issue

--- lib/dbservice_web/views/enrollment_record_view.ex
+++ lib/dbservice_web/views/enrollment_record_view.ex
@@ -14,11 +14,9 @@ defmodule DbserviceWeb.EnrollmentRecordView do
     %{
       id: enrollment_record.id,
       grade: enrollment_record.grade,
-      academic_year: enrollment_record.academic_year,
       is_current: enrollment_record.is_current,
       student_id: enrollment_record.student_id,
       school_id: enrollment_record.school_id,
-      board_medium: enrollment_record.board_medium,
       date_of_school_enrollment: enrollment_record.date_of_school_enrollment,
       date_of_group_enrollment: enrollment_record.date_of_group_enrollment,
       group_id: enrollment_record.group_id
