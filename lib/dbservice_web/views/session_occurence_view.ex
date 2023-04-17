4d42245ecad6451c67146d6d81f316f6fb5e626d
shubham belwal <shubhambelwal@coloredcows-iMac.local>
2023-04-17 15:51:32 +0530

    Fix error caused by incorrect Ecto preload in session_occurence view

--- lib/dbservice_web/views/session_occurence_view.ex
+++ lib/dbservice_web/views/session_occurence_view.ex
@@ -1,15 +1,13 @@
 defmodule DbserviceWeb.SessionOccurenceView do
   use DbserviceWeb, :view
   alias DbserviceWeb.SessionOccurenceView
-  alias DbserviceWeb.UserView
-  alias Dbservice.Repo
 
   def render("index.json", %{session_occurence: session_occurence}) do
     render_many(session_occurence, SessionOccurenceView, "session_occurence.json")
   end
 
   def render("show.json", %{session_occurence: session_occurence}) do
-    render_one(session_occurence, SessionOccurenceView, "session_occurence_with_users.json")
+    render_one(session_occurence, SessionOccurenceView, "session_occurence.json")
   end
 
   def render("session_occurence.json", %{session_occurence: session_occurence}) do
@@ -19,19 +17,6 @@ defmodule DbserviceWeb.SessionOccurenceView do
       end_time: session_occurence.end_time,
       session_fk: session_occurence.session_fk,
       session_id: session_occurence.session_id
-    }
-  end
-
-  def render("session_occurence_with_users.json", %{session_occurence: session_occurence}) do
-    session_occurence = session_occurence |> Repo.preload(:users)
-
-    %{
-      id: session_occurence.id,
-      start_time: session_occurence.start_time,
-      end_time: session_occurence.end_time,
-      session_fk: session_occurence.session_fk,
-      session_id: session_occurence.session_id,
-      users: render_many(session_occurence.users, UserView, "user.json")
     }
   end
 end
