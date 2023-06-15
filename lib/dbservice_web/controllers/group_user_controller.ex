2f3a04e54fcfe373ec592e1a3d3bdedd02c1204b
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2023-06-15 12:01:29 +0530

    added group user APIs

---
+++ lib/dbservice_web/controllers/group_user_controller.ex
@@ -0,0 +1,114 @@
+defmodule DbserviceWeb.GroupUserController do
+  use DbserviceWeb, :controller
+
+  import Ecto.Query
+  alias Dbservice.Repo
+  alias Dbservice.GroupUsers
+  alias Dbservice.Groups.GroupUser
+
+  action_fallback(DbserviceWeb.FallbackController)
+
+  use PhoenixSwagger
+
+  swagger_path :index do
+    get("/api/group-user")
+    response(200, "OK", Schema.ref(:GroupUsers))
+  end
+
+  def index(conn, params) do
+    query =
+      from(m in GroupUser,
+        order_by: [asc: m.id],
+        offset: ^params["offset"],
+        limit: ^params["limit"]
+      )
+
+    query =
+      Enum.reduce(params, query, fn {key, value}, acc ->
+        case String.to_existing_atom(key) do
+          :offset -> acc
+          :limit -> acc
+          atom -> from(u in acc, where: field(u, ^atom) == ^value)
+        end
+      end)
+
+    group_user = Repo.all(query)
+    render(conn, "index.json", group_user: group_user)
+  end
+
+  swagger_path :create do
+    post("/api/group-user")
+
+    parameters do
+      body(:body, Schema.ref(:GroupUsers), "Group user to create", required: true)
+    end
+
+    response(201, "Created", Schema.ref(:GroupUsers))
+  end
+
+  def create(conn, params) do
+    with {:ok, %GroupUser{} = group_user} <-
+           GroupUsers.create_group_user(params) do
+      conn
+      |> put_status(:created)
+      |> put_resp_header(
+        "location",
+        Routes.group_user_path(conn, :show, group_user)
+      )
+      |> render("show.json", group_user: group_user)
+    end
+  end
+
+  swagger_path :show do
+    get("/api/group-user/{groupUserId}")
+
+    parameters do
+      groupUserId(:path, :integer, "The id of the group user record", required: true)
+    end
+
+    response(200, "OK", Schema.ref(:GroupUsers))
+  end
+
+  def show(conn, %{"id" => id}) do
+    group_user = GroupUsers.get_group_user!(id)
+    render(conn, "show.json", group_user: group_user)
+  end
+
+  swagger_path :update do
+    patch("/api/group-user/{groupUserId}")
+
+    parameters do
+      groupUserId(:path, :integer, "The id of the session user", required: true)
+      body(:body, Schema.ref(:GroupUsers), "Group user to create", required: true)
+    end
+
+    response(200, "Updated", Schema.ref(:GroupUsers))
+  end
+
+  def update(conn, params) do
+    group_user = GroupUsers.get_group_user!(params["id"])
+
+    with {:ok, %GroupUser{} = group_user} <-
+           GroupUsers.update_group_user(group_user, params) do
+      render(conn, "show.json", group_user: group_user)
+    end
+  end
+
+  swagger_path :delete do
+    PhoenixSwagger.Path.delete("/api/group-user/{groupUserId}")
+
+    parameters do
+      groupUserId(:path, :integer, "The id of the group user record", required: true)
+    end
+
+    response(204, "No Content")
+  end
+
+  def delete(conn, params) do
+    group_user = GroupUsers.get_group_user!(params["id"])
+
+    with {:ok, %GroupUser{}} <- GroupUsers.delete_group_user(group_user) do
+      send_resp(conn, :no_content, "")
+    end
+  end
+end
---
+++ lib/dbservice_web/views/group_user_view.ex
@@ -0,0 +1,23 @@
+defmodule DbserviceWeb.GroupUserView do
+  use DbserviceWeb, :view
+  alias DbserviceWeb.GroupUserView
+
+  def render("index.json", %{group_user: group_user}) do
+    render_many(group_user, GroupUserView, "group_user.json")
+  end
+
+  def render("show.json", %{group_user: group_user}) do
+    render_one(group_user, GroupUserView, "group_user.json")
+  end
+
+  def render("group_user.json", %{group_user: group_user}) do
+    %{
+      id: group_user.id,
+      program_date_of_joining: group_user.program_date_of_joining,
+      program_student_language: group_user.program_student_language,
+      group_type_id: group_user.group_type_id,
+      user_id: group_user.user_id,
+      program_manager_id: group_user.program_manager_id
+    }
+  end
+end
