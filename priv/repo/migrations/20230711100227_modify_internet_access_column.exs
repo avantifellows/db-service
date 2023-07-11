107e932d66e9ec144c7b1d9b21b8717be44ffe14
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2023-07-11 15:33:57 +0530

    added db columns

---
+++ priv/repo/migrations/20230711100227_modify_internet_access_column.exs
@@ -0,0 +1,9 @@
+defmodule Dbservice.Repo.Migrations.ModifyInternetAccessColumn do
+  use Ecto.Migration
+
+  def change do
+    alter table(:student) do
+      modify(:has_internet_access, :string)
+    end
+  end
+end
