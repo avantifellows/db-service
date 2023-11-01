a70dfe12769c03a11968a816aa63dc20253c72b1
suryabulusu <teja.surya59@gmail.com>
2023-11-01 15:44:04 +0530

    adds router

--- lib/dbservice_web/router.ex
+++ lib/dbservice_web/router.ex
@@ -23,6 +23,25 @@ defmodule DbserviceWeb.Router do
     resources("/teacher", TeacherController, except: [:new, :edit])
     post("/teacher/register", TeacherController, :register)
     patch("/teacher/register/:id", TeacherController, :update_teacher_with_user)
+    resources("/user-profile", UserProfileController, only: [:index, :create, :update, :show])
+    resources("/student-profile", StudentProfileController, except: [:new, :edit])
+    post("/student-profile/setup", StudentProfileController, :setup)
+
+    patch(
+      "/student-profile/setup/:id",
+      StudentProfileController,
+      :update_student_profile_with_user_profile
+    )
+
+    resources("/teacher-profile", TeacherProfileController, except: [:new, :edit])
+    post("/teacher-profile/setup", TeacherProfileController, :setup)
+
+    patch(
+      "/teacher-profile/setup/:id",
+      TeacherProfileController,
+      :update_teacher_profile_with_user_profile
+    )
+
     resources("/school", SchoolController, except: [:new, :edit])
     resources("/enrollment-record", EnrollmentRecordController, except: [:new, :edit])
     resources("/session", SessionController, only: [:index, :create, :update, :show])
