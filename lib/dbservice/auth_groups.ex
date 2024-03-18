0dc30d372aa168c5f6adb70ce450f80c5c0aabdc
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2024-03-18 15:56:13 +0530

    formatting error

--- lib/dbservice/auth_groups.ex
+++ lib/dbservice/auth_groups.ex
@@ -56,7 +56,7 @@ defmodule Dbservice.AuthGroups do
     %AuthGroup{}
     |> AuthGroup.changeset(attrs)
     |> Ecto.Changeset.put_assoc(:group, [
-      %Group{type: "auth_group", child_id: attrs["id"]}
+      %AuthGroup{type: "auth_group", child_id: attrs["id"]}
     ])
     |> Repo.insert()
   end
