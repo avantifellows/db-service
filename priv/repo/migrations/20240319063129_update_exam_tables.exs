a78737dfbcbbeed8498818628cd63ac325db9b1c
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2024-03-19 12:10:04 +0530

    exam + student_exam_record

---
+++ priv/repo/migrations/20240319063129_update_exam_tables.exs
@@ -0,0 +1,24 @@
+defmodule Dbservice.Repo.Migrations.UpdateExamTables do
+  use Ecto.Migration
+
+  def change do
+    alter table(:exam) do
+      add :cutoff, :map
+
+      modify :name, :string, null: false
+    end
+
+    alter table(:student_exam_record) do
+      remove :rank
+
+      add :percentile, :float
+      add :all_india_rank, :integer
+      add :category_rank, :integer
+
+      modify :student_id, :integer, null: false
+      modify :exam_id, :integer,  null: false
+      modify :date, :date, null: false
+    end
+
+  end
+end
