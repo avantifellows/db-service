10a0b9bec9b9242afb46740d27e58eb65e56190c
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2024-05-25 14:40:28 +0530

    user_id from str > int

--- priv/repo/migrations/20240525065205_update_user_session.exs
+++ priv/repo/migrations/20240525065205_update_user_session.exs
@@ -6,6 +6,8 @@ defmodule Dbservice.Repo.Migrations.UpdateUserSession do
       add :user_activity_type, :string
       remove :session_occurrence_id
       add :session_id, references(:session)
+      remove :user_id
+      add :user_id, references(:user)
     end
   end
 end
