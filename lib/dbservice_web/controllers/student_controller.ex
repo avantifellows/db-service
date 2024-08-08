8d0e5bf7a3727fc7c8f65ecf1ba74455f5a1c814
Abhinav-3009 <abhi30092000.ag@gmail.com>
2024-08-08 16:12:54 +0530

    student_controller formatted

--- lib/dbservice_web/controllers/student_controller.ex
+++ lib/dbservice_web/controllers/student_controller.ex
@@ -579,7 +579,9 @@ defmodule DbserviceWeb.StudentController do
     post("/api/student/verify-student")
 
     parameters do
-      body(:body, Schema.ref(:VerifyStudentRequest), "Parameters needed to verify student", required: true)
+      body(:body, Schema.ref(:VerifyStudentRequest), "Parameters needed to verify student",
+        required: true
+      )
     end
 
     response(200, "OK", Schema.ref(:VerificationResult))
