8fb78e968cdc39ba9458ebb259563b42ec32c8c2
Aman Bahuguna <amanbahuguna009@gmail.com>
2025-01-04 15:32:04 +0530

    treat grade as a group

--- lib/dbservice/grades/grade.ex
+++ lib/dbservice/grades/grade.ex
@@ -7,6 +7,8 @@ defmodule Dbservice.Grades.Grade do
   alias Dbservice.Tags.Tag
   alias Dbservice.Chapters.Chapter
   alias Dbservice.Topics.Topic
+  alias Dbservice.Groups.Group
+  alias Dbservice.EnrollmentRecords.EnrollmentRecord
 
   schema "grade" do
     field(:number, :integer)
@@ -16,6 +18,12 @@ defmodule Dbservice.Grades.Grade do
     has_many(:chapter, Chapter)
     has_many(:topic, Topic)
     belongs_to(:tag, Tag)
+    has_many(:group, Group, foreign_key: :child_id, where: [type: "grade"])
+
+    has_many(:enrollment_record, EnrollmentRecord,
+      foreign_key: :group_id,
+      where: [group_type: "group"]
+    )
   end
 
   @doc false
