1444c4f2ebd0643869ef82a38a4500047f0069ca
Aman Bahuguna <amanbahuguna009@gmail.com>
2025-02-25 16:55:20 +0530

    remove curriculum id

--- priv/repo/migrations/20250205120105_migrate_chapter_curriculum_data.exs
+++ priv/repo/migrations/20250205120105_migrate_chapter_curriculum_data.exs
@@ -9,9 +9,17 @@ defmodule Dbservice.Repo.Migrations.MigrateChapterCurriculumData do
     FROM chapter
     WHERE curriculum_id IS NOT NULL;
     """
+
+    alter table(:chapter) do
+      remove :curriculum_id
+    end
   end
 
   def down do
+    alter table(:chapter) do
+      add :curriculum_id, references(:curriculum, on_delete: :nothing)
+    end
+
     # In case we need to rollback, we'll clear the chapter_curriculum table
     execute """
     DELETE FROM chapter_curriculum;
