9465e55e36a89c06f0789c007962b75f5512ea31
ycantistudy <ycantistudy@gmail.com>
2025-04-29 15:10:15 +0000

    reorganized and cleaned

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Dbservice.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix] ++ Mix.compilers() ++ [:phoenix_swagger],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -32,41 +33,36 @@ defmodule Dbservice.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.10"},
-      {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.10"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 4.2"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 1.0"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.8.2"},
-      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.3", runtime: Mix.env() == :dev},
-      {:swoosh, "~> 1.3"},
-      {:finch, "~> 0.13"},
-      {:telemetry_metrics, "~> 1.1"},
-      {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.26.0"},
-      {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
-      {:phoenix_swagger, "~> 0.8"},
-      {:ex_json_schema, "~> 0.10"},
-      {:faker, "~> 0.17", only: [:test, :dev]},
-      {:ex_check, "~> 0.16", only: [:dev], runtime: false},
-      {:dialyxir, ">= 0.0.0", only: [:dev], runtime: false},
-      {:credo, ">= 0.0.0", only: [:dev], runtime: false},
-      {:dotenvy, "~> 1.1"},
-      {:cors_plug, "~> 3.0"},
-      {:logger_file_backend, "~> 0.0.13"},
       {:calendar, "~> 1.0.0"},
-      {:observer_cli, "~> 1.7"},
-      {:oban, "~> 2.13"},
-      {:csv, "~> 3.0"},
-      {:httpoison, "~> 2.2"},
-      {:hackney, "~> 1.23"},
+      {:cors_plug, "~> 3.0.3"},
+      {:credo, "~> 1.7.12", only: [:dev], runtime: false},
+      {:csv, "~> 3.2.2"},
+      {:dialyxir, "~> 1.4.5", only: [:dev], runtime: false},
+      {:dotenvy, "~> 1.1.0"},
+      {:ecto_sql, "~> 3.12.0"},
+      {:esbuild, "~> 0.9.0", runtime: Mix.env() == :dev},
+      {:ex_check, "~> 0.16.0", only: [:dev], runtime: false},
+      {:ex_json_schema, "~> 0.10.0"},
+      {:faker, "~> 0.18.0", only: [:test, :dev]},
+      {:gettext, "~> 0.26.0"},
       {:google_api_sheets, "~> 0.29.0"},
-      {:goth, "~> 1.3"}
+      {:goth, "~> 1.4.5"},
+      {:hackney, "~> 1.23.0"},
+      {:httpoison, "~> 2.2"},
+      {:jason, "~> 1.4.4"},
+      {:logger_file_backend, "~> 0.0.13"},
+      {:oban, "~> 2.18.0"}, # 2.19 needs elixir 1.15
+      {:observer_cli, "~> 1.8.3"},
+      {:postgrex, "~> 0.20.0"},
+      {:phoenix, "~> 1.7.10"},
+      {:phoenix_ecto, "~> 4.6.3"},
+      {:phoenix_live_dashboard, "~> 0.8.7"},
+      {:swoosh, "~> 1.18.4"},
+      {:tailwind, "~> 0.3.1", runtime: Mix.env() == :dev},
+      {:telemetry_metrics, "~> 1.1.0"},
+      {:telemetry_poller, "~> 1.2.0"},
+      {:plug_cowboy, "~> 2.7.3"},
+      {:phoenix_swagger, "~> 0.8.2"},
     ]
   end
 
@@ -78,13 +74,14 @@ defmodule Dbservice.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
+      setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
-      "assets.build": ["tailwind default", "esbuild default"],
-      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
+      "assets.deploy": [
+        "tailwind dbservice --minify",
+        "phx.digest"
+      ]
     ]
   end
 end
