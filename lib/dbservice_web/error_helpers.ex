9e7efd67a40f0564d286c01dd0a40c3ba509356c
Aman Bahuguna <amanbahuguna009@gmail.com>
2025-05-26 15:15:17 +0530

    removed inspect statements

--- lib/dbservice_web/error_helpers.ex
+++ lib/dbservice_web/error_helpers.ex
@@ -5,8 +5,6 @@ defmodule DbserviceWeb.ErrorHelpers do
   Formats changeset errors for JSON response.
   """
   def errors_for_changeset(changeset) do
-    IO.inspect(changeset, label: "changeset")
-
     traverse_errors(changeset, fn {message, opts} ->
       Enum.reduce(opts, message, fn {key, value}, acc ->
         String.replace(acc, "%{#{key}}", inspect(value))
