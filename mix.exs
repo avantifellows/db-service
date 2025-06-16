a1a5af766e5e09a2bbff012e280d19ab2d5640b3
im-vedant <194vedantgupta@gmail.com>
2025-06-16 18:09:30 +0530

    fix : double form submission

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Dbservice.MixProject do
     [
       app: :dbservice,
       version: "0.1.0",
-      elixir: "~> 1.18.3",
+      elixir: "~> 1.18.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
