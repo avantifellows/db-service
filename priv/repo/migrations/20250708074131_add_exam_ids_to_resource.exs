ebb507b0226957a8f40ba10807d80e6f5b05cc52
Aman Bahuguna <amanbahuguna009@gmail.com>
2025-07-20 12:14:53 +0530

    rm data migration code

--- priv/repo/migrations/20250708074131_add_exam_ids_to_resource.exs
+++ priv/repo/migrations/20250708074131_add_exam_ids_to_resource.exs
@@ -8,17 +8,6 @@ defmodule Dbservice.Repo.Migrations.AddExamIdsToResource do
       add :code, :string
     end
 
-    # Data migration: Copy exam_ids from tag_ids by matching tag name to exam name
-    execute """
-    UPDATE resource SET exam_ids = (
-      SELECT array_agg(exam.id)
-      FROM unnest(tag_ids) AS tag_id
-      JOIN tag ON tag.id = tag_id
-      JOIN exam ON lower(exam.name) = lower(tag.name)
-    )
-    WHERE tag_ids IS NOT NULL AND array_length(tag_ids, 1) > 0;
-    """
-
     # Clean up tag_ids after migration
     execute """
     UPDATE resource SET tag_ids = NULL;
