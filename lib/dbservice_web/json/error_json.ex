e8f6a0c15a102c59b3e01ad23ddb841051165ae7
im-vedant <194vedantgupta@gmail.com>
2025-08-07 16:56:14 +0530

    add render/2 functions for the error codes

--- lib/dbservice_web/json/error_json.ex
+++ lib/dbservice_web/json/error_json.ex
@@ -1,9 +1,21 @@
 defmodule DbserviceWeb.ErrorJSON do
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.json", _assigns) do
-  #   %{errors: %{detail: "Internal Server Error"}}
-  # end
+  def render("500.json", _assigns) do
+    %{errors: %{detail: "Internal Server Error"}}
+  end
+
+  # Optionally, add other error handlers:
+  def render("404.json", _assigns) do
+    %{errors: %{detail: "Not Found"}}
+  end
+
+  def render("400.json", _assigns) do
+    %{errors: %{detail: "Bad Request"}}
+  end
+
+  # Fallback for any other status
+  def render(_template, _assigns) do
+    %{errors: %{detail: "Unknown error"}}
+  end
 
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
