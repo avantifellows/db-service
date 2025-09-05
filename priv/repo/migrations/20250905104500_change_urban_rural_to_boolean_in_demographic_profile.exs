6c084e4f932d934eb5f64b1bab4b25d8c3453114
saltykheera <vishal.kumar-ug23@nsut.ac.in>
2025-09-05 11:08:43 +0530

    refactor: clean up whitespace in migration for urban_rural boolean conversion

--- priv/repo/migrations/20250905104500_change_urban_rural_to_boolean_in_demographic_profile.exs
+++ priv/repo/migrations/20250905104500_change_urban_rural_to_boolean_in_demographic_profile.exs
@@ -9,26 +9,26 @@ defmodule Dbservice.Repo.Migrations.ChangeUrbanRuralToBooleanInDemographicProfil
 
     # Update the temporary column based on the string values
     execute """
-    UPDATE demographic_profile 
-    SET urban_rural_temp = CASE 
+    UPDATE demographic_profile
+    SET urban_rural_temp = CASE
       WHEN LOWER(urban_rural) IN ('urban', 'true', '1', 'yes') THEN true
       WHEN LOWER(urban_rural) IN ('rural', 'false', '0', 'no') THEN false
       ELSE false
     END
     """
-    
+
     # Drop the old column and rename the temporary one
     alter table(:demographic_profile) do
       remove :urban_rural
     end
-    
+
     alter table(:demographic_profile) do
       add :urban_rural, :boolean, default: false
     end
-    
+
     # Copy data from temp column to the new boolean column
     execute "UPDATE demographic_profile SET urban_rural = urban_rural_temp"
-    
+
     # Drop the temporary column
     alter table(:demographic_profile) do
       remove :urban_rural_temp
@@ -40,25 +40,25 @@ defmodule Dbservice.Repo.Migrations.ChangeUrbanRuralToBooleanInDemographicProfil
     alter table(:demographic_profile) do
       add :urban_rural_temp, :string
     end
-    
+
     execute """
-    UPDATE demographic_profile 
-    SET urban_rural_temp = CASE 
+    UPDATE demographic_profile
+    SET urban_rural_temp = CASE
       WHEN urban_rural = true THEN 'urban'
       ELSE 'rural'
     END
     """
-    
+
     alter table(:demographic_profile) do
       remove :urban_rural
     end
-    
+
     alter table(:demographic_profile) do
       add :urban_rural, :string
     end
-    
+
     execute "UPDATE demographic_profile SET urban_rural = urban_rural_temp"
-    
+
     alter table(:demographic_profile) do
       remove :urban_rural_temp
     end
