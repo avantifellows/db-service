a86700c51559586c80b70bbfabe45fe99e72a3a4
saltykheera <vishal.kumar-ug23@nsut.ac.in>
2025-09-11 16:07:58 +0530

    added examples and contexts

--- lib/dbservice/branches.ex
+++ lib/dbservice/branches.ex
@@ -10,6 +10,11 @@ defmodule Dbservice.Branches do
 
   @doc """
   Returns the list of branches.
+
+  ## Examples
+
+      iex> list_branch()
+      [%Branch{}, ...]
   """
   def list_branch do
     Repo.all(Branch)
@@ -17,6 +22,16 @@ defmodule Dbservice.Branches do
 
   @doc """
   Gets a single branch.
+
+  Raises `Ecto.NoResultsError` if the branch does not exist.
+
+  ## Examples
+
+      iex> get_branch!(123)
+      %Branch{}
+
+      iex> get_branch!(456)
+      ** (Ecto.NoResultsError)
   """
   def get_branch!(id) do
     Repo.get!(Branch, id)
@@ -24,6 +39,14 @@ defmodule Dbservice.Branches do
 
   @doc """
   Gets a branch by branch_id.
+
+  ## Examples
+
+      iex> get_branch_by_branch_id(1234)
+      %Branch{}
+
+      iex> get_branch_by_branch_id(9999)
+      nil
   """
   def get_branch_by_branch_id(branch_id) do
     Repo.get_by(Branch, branch_id: branch_id)
@@ -31,6 +54,14 @@ defmodule Dbservice.Branches do
 
   @doc """
   Creates a branch.
+
+  ## Examples
+
+      iex> create_branch(%{field: value})
+      {:ok, %Branch{}}
+
+      iex> create_branch(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
   """
   def create_branch(attrs \\ %{}) do
     %Branch{}
@@ -40,6 +71,14 @@ defmodule Dbservice.Branches do
 
   @doc """
   Updates a branch.
+
+  ## Examples
+
+      iex> update_branch(branch, %{field: new_value})
+      {:ok, %Branch{}}
+
+      iex> update_branch(branch, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
   """
   def update_branch(%Branch{} = branch, attrs) do
     branch
@@ -49,6 +88,14 @@ defmodule Dbservice.Branches do
 
   @doc """
   Deletes a branch.
+
+  ## Examples
+
+      iex> delete_branch(branch)
+      {:ok, %Branch{}}
+
+      iex> delete_branch(branch)
+      {:error, %Ecto.Changeset{}}
   """
   def delete_branch(%Branch{} = branch) do
     Repo.delete(branch)
@@ -56,6 +103,11 @@ defmodule Dbservice.Branches do
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking branch changes.
+
+  ## Examples
+
+      iex> change_branch(branch)
+      %Ecto.Changeset{data: %Branch{}}
   """
   def change_branch(%Branch{} = branch, attrs \\ %{}) do
     Branch.changeset(branch, attrs)
--- lib/dbservice/cutoffs.ex
+++ lib/dbservice/cutoffs.ex
@@ -10,6 +10,11 @@ defmodule Dbservice.Cutoffs do
 
   @doc """
   Returns the list of cutoffs.
+
+  ## Examples
+
+      iex> list_cutoffs()
+      [%Cutoff{}, ...]
   """
   def list_cutoffs do
     Repo.all(Cutoff)
@@ -17,6 +22,16 @@ defmodule Dbservice.Cutoffs do
 
   @doc """
   Gets a single cutoff.
+
+  Raises `Ecto.NoResultsError` if the cutoff does not exist.
+
+  ## Examples
+
+      iex> get_cutoff!(123)
+      %Cutoff{}
+
+      iex> get_cutoff!(456)
+      ** (Ecto.NoResultsError)
   """
   def get_cutoff!(id) do
     Repo.get!(Cutoff, id)
@@ -24,6 +39,14 @@ defmodule Dbservice.Cutoffs do
 
   @doc """
   Gets cutoffs by exam_occurrence_id.
+
+  ## Examples
+
+      iex> get_cutoffs_by_exam_occurrence_id(1)
+      [%Cutoff{}, ...]
+
+      iex> get_cutoffs_by_exam_occurrence_id(999)
+      []
   """
   def get_cutoffs_by_exam_occurrence_id(exam_occurrence_id) do
     Cutoff
@@ -33,6 +56,14 @@ defmodule Dbservice.Cutoffs do
 
   @doc """
   Gets cutoffs by college_id.
+
+  ## Examples
+
+      iex> get_cutoffs_by_college_id(1)
+      [%Cutoff{}, ...]
+
+      iex> get_cutoffs_by_college_id(999)
+      []
   """
   def get_cutoffs_by_college_id(college_id) do
     Cutoff
@@ -42,6 +73,14 @@ defmodule Dbservice.Cutoffs do
 
   @doc """
   Gets cutoffs by branch_id.
+
+  ## Examples
+
+      iex> get_cutoffs_by_branch_id(1)
+      [%Cutoff{}, ...]
+
+      iex> get_cutoffs_by_branch_id(999)
+      []
   """
   def get_cutoffs_by_branch_id(branch_id) do
     Cutoff
@@ -51,6 +90,14 @@ defmodule Dbservice.Cutoffs do
 
   @doc """
   Gets cutoffs by category.
+
+  ## Examples
+
+      iex> get_cutoffs_by_category("General")
+      [%Cutoff{}, ...]
+
+      iex> get_cutoffs_by_category("Unknown")
+      []
   """
   def get_cutoffs_by_category(category) do
     Cutoff
@@ -60,6 +107,16 @@ defmodule Dbservice.Cutoffs do
 
   @doc """
   Gets cutoffs with all associations preloaded.
+
+  Raises `Ecto.NoResultsError` if the cutoff does not exist.
+
+  ## Examples
+
+      iex> get_cutoff_with_associations!(123)
+      %Cutoff{exam_occurrence: %ExamOccurrence{}, college: %College{}, branch: %Branch{}}
+
+      iex> get_cutoff_with_associations!(456)
+      ** (Ecto.NoResultsError)
   """
   def get_cutoff_with_associations!(id) do
     Cutoff
@@ -69,6 +126,14 @@ defmodule Dbservice.Cutoffs do
 
   @doc """
   Creates a cutoff.
+
+  ## Examples
+
+      iex> create_cutoff(%{field: value})
+      {:ok, %Cutoff{}}
+
+      iex> create_cutoff(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
   """
   def create_cutoff(attrs \\ %{}) do
     %Cutoff{}
@@ -78,6 +143,14 @@ defmodule Dbservice.Cutoffs do
 
   @doc """
   Updates a cutoff.
+
+  ## Examples
+
+      iex> update_cutoff(cutoff, %{field: new_value})
+      {:ok, %Cutoff{}}
+
+      iex> update_cutoff(cutoff, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
   """
   def update_cutoff(%Cutoff{} = cutoff, attrs) do
     cutoff
@@ -87,6 +160,14 @@ defmodule Dbservice.Cutoffs do
 
   @doc """
   Deletes a cutoff.
+
+  ## Examples
+
+      iex> delete_cutoff(cutoff)
+      {:ok, %Cutoff{}}
+
+      iex> delete_cutoff(cutoff)
+      {:error, %Ecto.Changeset{}}
   """
   def delete_cutoff(%Cutoff{} = cutoff) do
     Repo.delete(cutoff)
@@ -94,6 +175,11 @@ defmodule Dbservice.Cutoffs do
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking cutoff changes.
+
+  ## Examples
+
+      iex> change_cutoff(cutoff)
+      %Ecto.Changeset{data: %Cutoff{}}
   """
   def change_cutoff(%Cutoff{} = cutoff, attrs \\ %{}) do
     Cutoff.changeset(cutoff, attrs)
