cfbe2f4c2c25affaf96e9d70863c1bcf6b7153fa
Aman Bahuguna <amanbahuguna009@gmail.com>
2025-10-23 13:24:03 +0530

    ci fixes

--- lib/dbservice_web/plugs/cache_plug.ex
+++ lib/dbservice_web/plugs/cache_plug.ex
@@ -1,54 +1,63 @@
 defmodule DbserviceWeb.CachePlug do
+  @moduledoc """
+  A plug that caches GET responses using Cachex for a configurable TTL (default: 3 minutes).
+
+  - Caches only `GET` requests.
+  - Emits telemetry events for cache hits/misses.
+  - Adds `x-cache` header (`HIT` or `MISS`).
+  """
+
   import Plug.Conn
 
   @default_ttl :timer.minutes(3)
 
   def init(opts), do: opts
 
-  def call(conn, opts) do
+  def call(%Plug.Conn{method: "GET"} = conn, opts) do
     ttl = Keyword.get(opts, :ttl, @default_ttl)
+    cache_key = build_cache_key(conn)
 
-    # Only cache GET requests
-    if conn.method == "GET" do
-      cache_key = build_cache_key(conn)
+    case Cachex.get(:dbservice_cache, cache_key) do
+      {:ok, nil} ->
+        handle_cache_miss(conn, cache_key, ttl)
 
-      case Cachex.get(:dbservice_cache, cache_key) do
-        {:ok, nil} ->
-          register_before_send(conn, fn conn ->
-            if conn.status == 200 and conn.resp_body != "" do
-              Cachex.put(:dbservice_cache, cache_key, conn.resp_body, ttl: ttl)
-
-              # Emit telemetry event for cache miss
-              :telemetry.execute(
-                [:dbservice, :cache, :miss],
-                %{count: 1},
-                %{key: cache_key, path: conn.request_path}
-              )
-            end
-
-            put_resp_header(conn, "x-cache", "MISS")
-          end)
-
-        {:ok, cached_body} ->
-          # Cache hit - return immediately
-          :telemetry.execute(
-            [:dbservice, :cache, :hit],
-            %{count: 1},
-            %{key: cache_key, path: conn.request_path}
-          )
-
-          conn
-          |> put_resp_content_type("application/json")
-          |> put_resp_header("x-cache", "HIT")
-          |> send_resp(200, cached_body)
-          |> halt()
-      end
-    else
-      conn
+      {:ok, cached_body} ->
+        handle_cache_hit(conn, cache_key, cached_body)
     end
   end
 
-  # Build deterministic cache key based on request path + query params
+  def call(conn, _opts), do: conn
+
+  defp handle_cache_hit(conn, cache_key, cached_body) do
+    :telemetry.execute(
+      [:dbservice, :cache, :hit],
+      %{count: 1},
+      %{key: cache_key, path: conn.request_path}
+    )
+
+    conn
+    |> put_resp_content_type("application/json")
+    |> put_resp_header("x-cache", "HIT")
+    |> send_resp(200, cached_body)
+    |> halt()
+  end
+
+  defp handle_cache_miss(conn, cache_key, ttl) do
+    register_before_send(conn, fn conn ->
+      if conn.status == 200 and conn.resp_body != "" do
+        Cachex.put(:dbservice_cache, cache_key, conn.resp_body, ttl: ttl)
+
+        :telemetry.execute(
+          [:dbservice, :cache, :miss],
+          %{count: 1},
+          %{key: cache_key, path: conn.request_path}
+        )
+      end
+
+      put_resp_header(conn, "x-cache", "MISS")
+    end)
+  end
+
   defp build_cache_key(conn) do
     "http:#{conn.request_path}?#{conn.query_string}"
   end
